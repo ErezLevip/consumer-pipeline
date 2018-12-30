333dc476b77f9845d452c4f8465c538486df39d2
Erez Levi <erez.levi.p@gmail.com>
2018-12-30 10:56:27 +0200

    errors metric update

--- errors_middleware.go
+++ errors_middleware.go
@@ -3,21 +3,30 @@ package consumer_pipeline
 import "log"
 
 func Errors(next MessageHandler) func(*MessageContext) {
-	return func(ctx *MessageContext) {
-		next(ctx)
-		reporter := ctx.Logger
-		errors := ctx.ctx.Value("errors")
+	return func(mctx *MessageContext) {
+		next(mctx)
+		reporter := mctx.Logger
+		errors := mctx.Ctx.Value("errors")
 		if errors == nil {
 			return
 		}
 
-		currentHandler := ctx.ctx.Value("current_handler").(string)
 		for _, err := range errors.([]*ErrorMetric) {
 			log.Println(err)
+			function := err.Function
+			if function == ""{
+				function = mctx.Ctx.Value("current_handler").(string)
+			}
+
+			context := err.Context
+			if context == ""{
+				context = mctx.Ctx.Value("topic").(string)
+			}
+
 			reporter.Send("errors",
 				1, map[string]string{
-					`context`:  ctx.ctx.Value("topic").(string),
-					`function`: currentHandler,
+					`context`:  context,
+					`function`: function,
 				})
 		}
 	}
